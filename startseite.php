592e7024ede7903fb2a472fe877e225364b6fd71
Halsohren <74795677+Halsohren@users.noreply.github.com>
2020-12-02 13:09:28 +0100

    Update startseite.php

--- startseite.php
+++ startseite.php
@@ -4,23 +4,18 @@
 </head>
 <body> 
 
+<form actiom="startseite.php" method="post">
 <?php 
 
-echo "<p>Name:</p>". $_POST['name'];
-
-echo "<p>Nachricht:</p>". $_POST['nachricht'];
+$name = $_POST["name"];
+$nachricht = $_POST["nachricht"];
+echo $name . ": " . $nachricht;
 ?>
 
-<form actiom="startseite.php" method="post">
 
-<p>Ihr Name:
- <input type="texet" name="name">
-</p>
-
+<p>Ihr Name: <input type="texet" name="name"></p>
 <p>Ihre Nachricht an uns:</p>
- <p>
- <textarea name="nachricht" type="text" cols="30" rows="5"></textarea>
-</p>
+<p><textarea name="nachricht" type="text" cols="30" rows="5"></textarea></p>
 
 <p><input type="submit" value="Senden"></p>
 
